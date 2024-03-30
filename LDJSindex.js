baff722a0b524ddcd07712f51ec0f57276e4c5e6
Jessie <140980383+JessieDane@users.noreply.github.com>
2024-03-30 12:02:37 +0100

    read files

--- LDJSindex.js
+++ LDJSindex.js
@@ -10,82 +10,82 @@
 //function list
 //  to be made
 //  
-//dices
-//spans
-let currentBetQuantitySpan = document.getElementById("currentBetquantity");
-let currentBetNumSpan = document.getElementById("currentBetNum");
-//player dices spans
-let dice1P = document.getElementById("dice1P");
-let dice2P = document.getElementById("dice2P");
-let dice3P = document.getElementById("dice3P");
-let dice4P = document.getElementById("dice4P");
-let dice5P = document.getElementById("dice5P");
-//Cpu dices spans
-let dice1Cpu = document.getElementById("dice1Cpu");
-let dice2Cpu = document.getElementById("dice2Cpu");
-let dice3Cpu = document.getElementById("dice3Cpu");
-let dice4Cpu = document.getElementById("dice4Cpu");
-let dice5Cpu = document.getElementById("dice5Cpu");
+//
+// let createSpan = document.createElement("span"); //move to forloop//markfordelete
 //dice array
-let tableArray = [];//length = amount of players * 5
-let tableArrayLengthVar = 9;
+let tableArray = [];//length = amount of players * 5 //the array to hold all dices
+let tableArrayLengthVar = 9;    //relates to setting up tabledices and dice displays
 //
 //random dice throw
-let randomDice;
+let randomDice;     // random number generator            
 //sets amount of points for winning condition
 let playerPoints = 4;
 let cpuPoints = 4;
-//who turn it is ( relates to decision making phase)
-let turnholder = 1;
-let gamePhase = 1;
+//
+let turnholder = 1; // relates to decision making 
+let gamePhase = 1;  // relates to buttons.disable
 //betting
 //currentbet
-let currentBetQuantityDice = 0;
-let currentBetNumDice = 0;
+let currentBetQuantityDice = 0; //quantity value of the placed bet
+let currentBetNumDice = 0;      //dice value of the placed bet
 //new bet
-let newBetQuantityDice;
-let newBetNumDice;
-let betholder;  
+let newBetQuantityDice;         //quantity value of proposed bet
+let newBetNumDice;              //dice value of proposed bet
+let betholder;                  //relates to determining the victor
 //call liar
 let challengeCounter = 0; // counts how many dices match
 let challenger; // is the one who initiates call liar.
 //layout
+//player span cpntainer
+let playerDiceContainer = document.getElementById("playerDice");
+//Cpu span container
+let cpuDiceContainer = document.getElementById("cpuDice");
 //buttons
-let startRoundBtn = document.getElementById("startGameButton");
-let quitBtn = document.getElementById("quit");
-let newBetBtn = document.getElementById("newBetButton");
+let startRoundBtn = document.getElementById("startGameButton"); 
+let quitBtn = document.getElementById("quit");                  
+let newBetBtn = document.getElementById("newBetButton");        
 let callLiarBtn = document.getElementById("callLiar");
 //inputfields
 let inputQuantity = document.getElementById("newBetQuantity");
 let inputNum = document.getElementById("newBetNum");
-
-
+//spans
+let currentBetQuantitySpan = document.getElementById("currentBetquantity"); 
+let currentBetNumSpan = document.getElementById("currentBetNum");           
+// forloop variables
+// i = set tableArray
+// j = set player dice cards
+// k = set cpu dice cards
+// l = show cpu dice during callLiarFunction
+//
+//on clicks
 startRoundBtn.onclick = setDiceFunction;    //button to throw dices
 quitBtn.onclick = quitFunction;             //button to quit..
 newBetBtn.onclick = makingBetFunction;      //button to make a bet
 callLiarBtn.onclick = callLiarFunction;     //button to call liar
-
+//
 // gameflow functions
 function setDiceFunction() {    //sets the dice into an array
     gamePhase = 1;              //button toggle
-    tableArray.length = 0;       // to clean tablearray
-    
+    tableArray.length = 0;      // to clean tablearray
+
+      
     for (let i = 0; i <= tableArrayLengthVar; i++) { //i < (amount players * 5)
         tableArray.push(Math.floor(Math.random() * 6 + 1));
     }
-    //use a for loop and append child to make this
-    //sets innertext to display
-    dice1P.innerText = tableArray[0];
-    dice2P.innerText = tableArray[1];
-    dice3P.innerText = tableArray[2];
-    dice4P.innerText = tableArray[3];
-    dice5P.innerText = tableArray[4];
-    
-    dice1Cpu.innerText = "?";
-    dice2Cpu.innerText = "?";
-    dice3Cpu.innerText = "?";
-    dice4Cpu.innerText = "?";
-    dice5Cpu.innerText = "?";
+   
+    for (let j = 0; j <= playerPoints; j++) {   //for loop to create dices to display appropriate to player points
+        let createSpan = document.createElement("span");
+        createSpan.classList.add("diceP");
+        playerDiceContainer.appendChild(createSpan);
+        createSpan.innerText = tableArray[`${j}`];
+    };
+        
+    for (let k = 0; k <= cpuPoints; k++) {      //for loop to create dices to display appropriate to cpu points
+        let createSpan = document.createElement("span");
+        createSpan.classList.add("diceCpu");
+        cpuDiceContainer.appendChild(createSpan);
+        createSpan.innerText = "?";
+    }
           
     gamePhase = 2;                  //button toggle
     if (gamePhase === 2){
@@ -95,36 +95,35 @@ function setDiceFunction() {    //sets the dice into an array
         inputNum.disabled = false;
     }
     console.log(tableArray);
-    console.log( "set board")        // mark for clearing
+    console.log( "set board");        // markfordelete
 };
-
+//
 function makingBetFunction() {      //tries to make a bet
     if (parseInt(inputQuantity.value) >= 1 && parseInt(inputNum.value) >= 1 ) {
         newBetQuantityDice = parseInt(inputQuantity.value);
         newBetNumDice = parseInt(inputNum.value);
-        if (newBetNumDice > currentBetNumDice ) {  // name ruleset = Reset
+        if (newBetNumDice > currentBetNumDice && newBetNumDice <= 6) {  // name ruleset = Reset
             currentBetQuantityDice = newBetQuantityDice;
             currentBetNumDice = newBetNumDice;
             currentBetQuantitySpan.innerText = currentBetQuantityDice+"x";
             currentBetNumSpan.innerText = currentBetNumDice+"'s";
             
-            betholder = 1;
+            betholder = 1;  
             turnholder = 0; // because now the other has to make a decision
             console.log(turnholder + " turnholder");
         };
     } else {
         alert("invalid entry, Agent Smith. please try again");
-    }
+    };
     if (gamePhase === 3) {
         newBetBtn.disabled = true;
         callLiarBtn.disabled = true;
-    }
-    cpuBehavior()
+    };
+    cpuBehavior();
 };
-
-function cpuBehavior() {
-     
-    console.log("trialcpubehavior");
+//
+function cpuBehavior() { // decision is 1/3 to call liar
+    console.log("cpu decides next move");
     if (turnholder === 0) { // if it is not, it will exit function and let player do things
         randomDiceThrow();  // random dice throw generator
         if( randomDice % 3 === 0 ) { // to decide cpu choices
@@ -136,31 +135,36 @@ function cpuBehavior() {
         }
     };
 };
-   
+//
 function callLiarFunction() { // will conclude the round
     newBetBtn.disabled = true;
     callLiarBtn.disabled = true;
     // all cards are shown
-        // manual adjustment
-    dice1Cpu.innerText = tableArray[5];
-    dice2Cpu.innerText = tableArray[6];
-    dice3Cpu.innerText = tableArray[7];
-    dice4Cpu.innerText = tableArray[8];
-    dice5Cpu.innerText = tableArray[9];
-    /* using the for loop already there, to create spans to diplay on the screen 
-    and have their numbers show, highlighting the ones that match*/ 
+    for (let l = 0; l <= cpuPoints; l++) { 
+        cpuDiceContainer.removeChild(cpuDiceContainer.children[0]);
+        let createSpan = document.createElement("span");
+        createSpan.classList.add("diceCpu");
+        cpuDiceContainer.appendChild(createSpan);
+        createSpan.innerText = tableArray[`${tableArrayLengthVar - l}`]; //displays the dice value from tableArray
+    };
+        //using the for loop already here, to create spans to diplay on the screen 
+        //and have their numbers show, highlighting the ones that match
+        //not completed!!!
     for ( let element of tableArray ) { //made variable to iterate over array 
-        if (element === currentBetNumDice){
+        if (element === currentBetNumDice) {
             challengeCounter++; // challenge counter ++ when a num is a match
         };
     };
-//////////// mark for cutting in new function /////////////////
+//////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////
+
     if ( challengeCounter < currentBetQuantityDice ) { // to check if the bet is good or not
         console.log("betholder loses"); // betholder loses the round challenger -1 dice
         startRoundBtn.disabled = false;
         tableArrayLengthVar = tableArrayLengthVar - 1;
         if ( betholder=1) {
-            document.getElementById("playerdice").lastChild.remove; // doesn't work yet
+            //document.getElementById("playerdice").lastChild.remove; // doesn't work yet
         };
         console.log(tableArray)
     } else  if (challengeCounter === currentBetQuantityDice) {
@@ -168,8 +172,8 @@ function callLiarFunction() { // will conclude the round
         startRoundBtn.disabled = false;
         tableArrayLengthVar = tableArrayLengthVar - 2;
         if ( betholder=0) {
-            for (let j = 0; j <= 1; j++ ){ 
-                document.getElementById("playerdice").lastChild.remove; // doesn't work yet
+            for (let j = 0; j <= 1; j++ ) { 
+                //document.getElementById("playerdice").lastChild.remove; // doesn't work yet
             };
         };
     } else {
@@ -177,71 +181,78 @@ function callLiarFunction() { // will conclude the round
         startRoundBtn.disabled = false;
         tableArrayLengthVar = tableArrayLengthVar - 1;
         if ( betholder = 0) {
-            document.getElementById("playerdice").lastChild.remove; // doesn't work yet
+            //document.getElementById("playerdice").lastChild.remove; // doesn't work yet
         };
     };
-/////////////////// mark for cutting new function ////////////////    
+/////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////  
     if(playerPoints !== 0 && cpuPoints !== 0) {
+        //playerDiceContainer.removeChild(playerDiceContainer.children); // to remove dices to make place for new
+        //cpuDiceContainer.removeChild(cpuDiceContainer.children);       // to remove dices to make place for new
+        // above is not functional yet
         gamePhase = 1;
-        tableArray.length = 0;
+        tableArray.length = 0; //erases current array to prepare for next round
         setDiceFunction();
     } else if (cpuPoints === 0) {
         alert("player won the game!");
     } else if (playerPoints === 0) {
-        alert("player lost the game!")
-    }
-    
+        alert("player lost the game!");
+    };
 };
-
+//
 function cpuNewBet() { //how cpu determines a bet // to be improved !
     gamePhase = 3;  //button disable toggle
     randomDiceThrow();
     newBetQuantityDice = randomDice;
     newBetNumDice = newBetNumDice + 1;
     
-    if (newBetNumDice > currentBetNumDice ) {  // name ruleset = Reset
+    if (newBetNumDice > currentBetNumDice && newBetNumDice <= 6) {  // name ruleset = Reset
         currentBetQuantityDice = newBetQuantityDice;
         currentBetNumDice = newBetNumDice;
         currentBetQuantitySpan.innerText = currentBetQuantityDice+"x";
         currentBetNumSpan.innerText = currentBetNumDice+"'s";
-    };
-    
+    } else {
+        retryBetFunction;
+         
+    }
     turnholder = 1; // because now the player has to make a decision
     console.log(turnholder + " turnholder");
     if (gamePhase === 3) {      
         newBetBtn.disabled = false;
         callLiarBtn.disabled = false;
-    }
+    };
 };
-
-// other functions 
-
+// other functions
+// retrybet
+function retryBetFunction() {
+    console.log("cpu has to try rolling again");
+    cpuNewBet;
+};
+// quit function 
 function quitFunction() { //quit button :)
-    alert("lol, no.");
+    alert("no.");
     console.log("user tried to leave..");
 };
-
+//
 function randomDiceThrow() {    // a dice throw can be divided in 2, 3, or 6 parts. 
     randomDice = Math.floor(Math.random() * 6 + 1);
     console.log(randomDice + " random dice");
 };
-
+//
 // function pathways
 /*this describes chain of functions ( fa => fb )
 
 */
-
 // process explanations
 /* turn holder
     helps to keep the functions apropriately.
 */
-
 /* bet holder
     serves as a way to help correctly pull a dice out of view from player.
     cpu doesn't matter, so you need to know who made that bet.
     if player made the bet, the bet holder = 1
 */
-
 /*gamePhase.
     game phase is a variable to help me toggle the buttons in various stages.
     --
@@ -277,7 +288,7 @@ function randomDiceThrow() {    // a dice throw can be divided in 2, 3, or 6 par
 
     when somebody loses a dice, that means they get 1 dice less in their cup.
     game is concluded when the last player remains.
-    */
+*/
 
 
 
